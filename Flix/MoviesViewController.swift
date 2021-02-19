1a9d2c634b23cbec9a198e0041f9e02b42ef12ad
Linxi Xu <xulinxi@sas.upenn.edu>
2021-02-19 02:20:28 -0500

    Update movie cell with basic info

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -9,8 +9,7 @@ import UIKit
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    
-    
+
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -54,13 +53,13 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
         
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
         
         
-        cell.textLabel!.text = title
+        cell.titleLabel.text = title
         
         return cell
     }
