371bf6ba3bcc15b5b11c1d0fdf47a6dd12d5fad3
Linxi Xu <xulinxi@sas.upenn.edu>
2021-02-19 02:08:12 -0500

    initialize moviecell

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Linxi Xu on 2/19/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
