4af23cbcfd415665f9fa7f92702104c6bebfd93d
Linxi Xu <xulinxi@sas.upenn.edu>
2021-02-22 00:46:18 -0500

    (bonus 1) Update movie details screen within the superhero movieDetailsTrailerViewController

--- Flix/MovieDetailsTrailerViewController.swift
+++ Flix/MovieDetailsTrailerViewController.swift
@@ -10,13 +10,39 @@ import AlamofireImage
 
 class MovieDetailsTrailerViewController: UIViewController {
     
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
     var movie: [String:Any]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print(movie["title"])
+//        print(movie["title"])
+        
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl +
+            posterPath)
+        
+        posterView.af_setImage(withURL: posterUrl!)
+        
+        
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" +
+                                backdropPath)
+        
+        backdropView.af_setImage(withURL: backdropUrl!)
     }
     
 
